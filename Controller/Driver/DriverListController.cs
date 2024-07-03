c89990756628b36aaae5eba42eb18b384cd3de3c
SalvatoreAmaddio <141591986+SalvatoreAmaddio@users.noreply.github.com>
2024-07-03 23:38:06 +0100

    update

--- Controller/Driver/DriverListController.cs
+++ Controller/Driver/DriverListController.cs
@@ -8,19 +8,42 @@ namespace SkipManagement.Controller
 {
     public class DriverListController : AbstractFormListController<Driver>
     {
-        public override AbstractClause InstantiateSearchQry() =>
-        new Driver().Select().All().From();
+        public DriverListController() 
+        {
+            OpenWindowOnNew = false;
+            AfterUpdate += OnAfterUpdate;
+        }
+
+        private async void OnAfterUpdate(object? sender, AfterUpdateArgs e)
+        {
+            if (!e.Is(nameof(Search))) return;
+            await OnSearchPropertyRequeryAsync(sender);
+        }
+
         public override void OnOptionFilterClicked(FilterEventArgs e)
         {
         }
 
-        public override Task<IEnumerable<Driver>> SearchRecordAsync()
+        public override async Task<IEnumerable<Driver>> SearchRecordAsync()
         {
-            throw new NotImplementedException();
+            SearchQry.AddParameter("name", Search.ToLower() + "%");
+            SearchQry.AddParameter("name", Search.ToLower() + "%");
+            return await CreateFromAsyncList(SearchQry.Statement(), SearchQry.Params());
         }
 
         protected override void Open(Driver model)
         {
         }
+
+        public override AbstractClause InstantiateSearchQry() =>
+        new Driver()
+            .Select().All()
+            .From()
+            .Where()
+                .OpenBracket()
+                    .Like("LOWER(FirstName)", "@name")
+                    .OR()
+                    .Like("LOWER(LastName)", "@name")
+                .CloseBracket();
     }
 }
